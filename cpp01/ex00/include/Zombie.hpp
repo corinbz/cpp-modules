78a45d8fadfaa124df0b34e44147feb751aa014a
corinbz <craciuncorin5@gmail.com>
2025-02-18 13:55:12 +0100

    added colors

--- cpp01/ex00/include/Zombie.hpp
+++ cpp01/ex00/include/Zombie.hpp
@@ -1,5 +1,11 @@
 #include <string>
 
+#define GREEN "\033[0;32m" 
+#define RED "\033[0;31m" 
+#define BLUE "\033[0;34m" 
+#define YELLOW "\033[0;33m" 
+#define RESET "\033[0m" 
+
 class Zombie
 {
 	private:
--- cpp01/ex00/src/Zombie.cpp
+++ cpp01/ex00/src/Zombie.cpp
@@ -4,16 +4,16 @@
 //constructor
 Zombie::Zombie(std::string name) : _name(name)
 {
-	std::cout << "Look out! " << _name << " is alive... well, undead!" << std::endl;
+	std::cout << GREEN << "Look out! " << _name << " is alive... well, undead!" << RESET << std::endl;
 }
 
 //destructor
 Zombie::~Zombie()
 {
-	std::cout << "Zombie " << _name << " has been destroyed. The world is safe... for now." << std::endl;
+	std::cout << RED <<"Zombie " << _name << " has been destroyed. The world is safe... for now." << RESET <<std::endl;
 }
 
 void Zombie::announce(void)
 {
-	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << YELLOW << _name << ": BraiiiiiiinnnzzzZ..." << RESET <<std::endl;
 }
