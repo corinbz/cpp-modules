6ceb0f362f4c2d4d469c75f0bd7fd2a9af07a487
Corin Craciun <craciuncorin5@gmail.com>
2025-04-24 22:35:31 +0200

    done cpp05/ex00

---
+++ cpp05/ex00/src/main.cpp
@@ -0,0 +1,49 @@
+#include "../include/Bureaucrat.hpp"
+#include "iostream"
+
+int main(void) {
+
+  {
+    try {
+      Bureaucrat John("John", 0);
+      std::cout << John << std::endl;
+    } catch (std::exception &e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+
+  {
+    try {
+      Bureaucrat John("John", 151);
+      std::cout << John << std::endl;
+    } catch (std::exception &e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+
+  {
+    try {
+      Bureaucrat John("John", 2);
+      std::cout << John << std::endl;
+      John.incrementGrade();
+      John.incrementGrade();
+      John.incrementGrade();
+    } catch (std::exception &e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+
+  {
+    try {
+      Bureaucrat John("John", 149);
+      std::cout << John << std::endl;
+      John.decrementGrade();
+      John.decrementGrade();
+      John.decrementGrade();
+    } catch (std::exception &e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+
+  return (0);
+}
