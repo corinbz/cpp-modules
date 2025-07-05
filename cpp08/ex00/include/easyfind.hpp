958d4e0a555f4e1cb4f25849ce8c882a9d3abd54
corinbz <craciuncorin5@gmail.com>
2025-07-05 11:03:11 +0200

    done ex00

--- cpp08/ex00/include/easyfind.hpp
+++ cpp08/ex00/include/easyfind.hpp
@@ -1,4 +1,12 @@
 #pragma once
 #include <algorithm>
 
-
+// template to find b in containter a
+// we assume T is a container of integers
+template <typename T> typename T::const_iterator easyfind(const T &a, int b) {
+  // return type will be an iterator.
+  // iterator type is just a pointer to the a value inside the container.
+  // iterator.end() point to the theoretical next element position. it's used to
+  // check if any value was found.
+  return (std::find(a.begin(), a.end(), b));
+}
--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -1,6 +1,23 @@
-#include "../include/templates.hpp"
+#include "../include/easyfind.hpp"
+#include <array>
 #include <iostream>
+#include <vector>
 
 int main() {
-return w;
+  int val = 1;
+
+  std::array<int, 3> arrr = {1, 2, 3};
+  std::array<int, 3>::const_iterator arr_it = easyfind(arrr, val);
+  if (arr_it != arrr.end())
+    std::cout << "Found it!" << std::endl;
+  else
+    std::cout << "Not found" << std::endl;
+
+  std::vector<int> vecc = {1, 2, 3};
+  std::vector<int>::const_iterator vec_it = easyfind(vecc, val);
+  if (vec_it != vecc.end())
+    std::cout << "Found it!" << std::endl;
+  else
+    std::cout << "Not found" << std::endl;
+  return 0;
 }
