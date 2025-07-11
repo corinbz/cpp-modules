cf9f52668f1eb89b805896dc50520767c742d36c
corinbz <craciuncorin5@gmail.com>
2025-07-11 09:55:39 +0200

    cpp09 boilerplate

---
+++ cpp09/ex02/include/PmergeMe.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <stack>
+
+template <class T>
+
+class MutantStack : public std::stack<T> {
+private:
+  std::stack<T> stack;
+
+public:
+  typedef typename std::stack<T>::container_type::iterator it;
+  MutantStack() : std::stack<T>() {};
+  MutantStack(MutantStack const &src) : std::stack<T>(src) { *this = src; };
+  MutantStack &operator=(MutantStack const &src) {
+    std::stack<T>::operator=(src);
+    return (*this);
+  };
+  ~MutantStack() {};
+  it begin() { return this->c.begin(); }
+  it end() { return this->c.end(); }
+};
