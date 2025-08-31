ca93c79f7be4f4e63c2437d72f47535a75809933
Corin Craciun <craciuncorin5@gmail.com>
2025-08-31 09:03:26 +0200

    small update

--- cpp09/ex00/include/BitcoinExchange.hpp
+++ cpp09/ex00/include/BitcoinExchange.hpp
@@ -1,18 +1,1 @@
 #pragma once
-
-#include <iostream>
-#include <stack>
-#include <string>
-
-class RPN {
-private:
-  std::stack<double> *stackarino_;
-
-public:
-  RPN();
-  RPN(std::stack<double> number);
-  RPN(const RPN &src);
-  ~RPN();
-  RPN &operator=(const RPN &src);
-  void solveIt(std::string input);
-};
--- cpp09/ex00/src/BitcoinExchange.cpp
+++ cpp09/ex00/src/BitcoinExchange.cpp
@@ -1,13 +1,1 @@
 #include "../include/BitcoinExchange.hpp"
-#include <stack>
-
-RPN::RPN() {};
-RPN::RPN(std::stack<double> numbers) : stackarino_(&numbers) {}
-RPN::RPN(const RPN &src) { *this = src; }
-RPN &RPN ::operator=(const RPN &src) {
-  if (this != &src) {
-    this->stackarino_ = src.stackarino_;
-  }
-  return *this;
-}
-RPN::~RPN() {};
